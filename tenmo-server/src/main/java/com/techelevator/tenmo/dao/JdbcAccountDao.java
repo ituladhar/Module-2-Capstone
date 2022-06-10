54c1dd1fb4545f12d2af5b947e7ce7abdfc2a45a
Ishwor Tuladhar <ishwor.tuladhar@gmail.com>
2022-06-10 18:27:16 -0400

    Worked on JdbcAccoutDao

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -1,12 +1,36 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.model.Account;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
 import java.math.BigDecimal;
 
 public class JdbcAccountDao implements AccountDao {
+    private JdbcTemplate jdbcTemplate;
 
 
     @Override
     public BigDecimal getBalance(int id) {
-        return null;
+        BigDecimal balance;
+        String sql = "SELECT balance " +
+                "FROM account " +
+                "JOIN tenmo_user ON account.user_id = tenmo_user.user_id " +
+                "WHERE tenmo_user.user_id = ?";
+        balance = jdbcTemplate.queryForObject(sql, BigDecimal.class, id);
+        return balance;
+    }
+
+    private Account mapRowToAccount(SqlRowSet rowSet) {
+        Account account = new Account();
+        account.setAccountId(rowSet.getInt("account_id"));
+        account.setUserId(rowSet.getInt("user_id"));
+        account.setBalance(rowSet.getBigDecimal("balance"));
+        return account;
+    }
+
+
+    public  JdbcAccountDao(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
     }
 }
