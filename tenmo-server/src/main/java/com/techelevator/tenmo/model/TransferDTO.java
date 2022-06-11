68a1e2229623600b04aed046587b47ebdebcec74
Ishwor Tuladhar <ishwor.tuladhar@gmail.com>
2022-06-10 21:18:23 -0400

    Need to work on sendTransfer and requestTransfer on JdbcAccountDao.

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/TransferDTO.java
@@ -0,0 +1,31 @@
+package com.techelevator.tenmo.model;
+
+public class TransferDTO {
+    private int fromUserId;
+    private int toUserId;
+    private double amount;
+
+    public int getFrom() {
+        return fromUserId;
+    }
+
+    public void setFrom(int from) {
+        this.fromUserId = from;
+    }
+
+    public int getTo() {
+        return toUserId;
+    }
+
+    public void setTo(int to) {
+        this.toUserId = to;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
